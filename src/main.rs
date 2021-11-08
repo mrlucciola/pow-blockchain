023c08b3a1cfd5c53b0141ae8a9da848c9aaf184
matthias <mrlucciola@protonmail.com>
2021-11-08 17:29:54 -0500

    added hashable to the Block struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use std::vec;
 
-use blockchainlib::{Block, now};
+use blockchainlib::{Block, now, Hashable};
 
 fn main () {
-    let block = Block::new(13, now(), vec![0, 32], 0, "Genesis block!".to_owned());
+    let mut block = Block::new(13, now(), vec![0, 32], 0, "Genesis block".to_owned());
     println!("{:?}", &block);
+
+    let h = block.hash();
+    println!("\n hash: {:?}", &h);
+    
+    block.hash = h;
+    println!("\n block: {:?}", &block);
 }
