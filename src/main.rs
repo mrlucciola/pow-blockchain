176c35952fc9e9f9dbc32faa66e8385124c2fa44
matthias <mrlucciola@protonmail.com>
2021-11-11 09:22:51 -0500

    added verification and tests after each block

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let mut block = Block::new(
         0,
         now(),
-        vec![0, 32],
+        vec![0; 32],
         0,
         "Genesis block".to_owned(),
         difficulty,
@@ -21,6 +21,8 @@ fn main() {
     let mut blockchain = Blockchain {
         blocks: vec![block],
     };
+
+    println!("Verify: {}", &blockchain.verify());
 
     for i in 1..=10 {
         let mut block = Block::new(
@@ -38,5 +40,9 @@ fn main() {
         last_hash = block.hash.clone();
         
         blockchain.blocks.push(block);
+
+        println!("Verify: {}", &blockchain.verify());
     }
+
+    println!("Verify: {}", &blockchain.verify());
 }
